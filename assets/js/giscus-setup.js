fe8ee4cfc9053a0ab5f4bc060688776185cb1a60
mllytle <61307057+mllytle@users.noreply.github.com>
2026-01-28 00:29:37 +0000

    Deploying to gh-pages from @ mllytle/mllytle.github.io@6c1c2ff894eb03f3300d366ce0c70e610a7aed28 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mllytle/madlytle.github.io",
+    "data-repo": "mllytle/mllytle.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
