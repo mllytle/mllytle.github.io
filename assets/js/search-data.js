0bd2978e50978c429fbc24dde4e540ca6223ca67
mllytle <61307057+mllytle@users.noreply.github.com>
2026-09-14 23:26:47 +0000

    Deploying to gh-pages from @ mllytle/mllytle.github.io@b585206b860776093fac94170701056b13b641b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,7 +402,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
+          section: "News",},{id: "projects-implicit-bending",
+          title: 'Implicit Bending',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -442,11 +447,6 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "projects-implicit-bending",
-          title: 'Implicit Bending',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/implicit_bending/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
